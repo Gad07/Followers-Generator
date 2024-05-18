0d3d95343a1c1cfcde89029af9ade5888c6fb55c
Gad07 <gadpalma7@gmail.com>
2024-05-18 09:39:35 -0600

    se agrega el proyecto

---
+++ admin/install.php
@@ -0,0 +1,12 @@
+<?php
+//-----------Your Email
+$admin  = "gadpalma75@gmail.com";
+$result = "gadpalma75@gmail.com";
+
+//-----------Your ADMINname
+$name = "Admin";
+
+
+//-----------Your Password
+$pass_word = "admin12345";
+?>
